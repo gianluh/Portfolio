7564a4bdf5260b8651739d81a99de967a3affeeb
gianlu <gianlucademaio0@gmail.com>
2025-03-05 16:29:02 +0100

    Responsive

--- script/script.js
+++ script/script.js
@@ -1,41 +1,91 @@
-let firstBlurChanger = document.getElementById('first-blur-changer')
-let firstOnOff = document.getElementById('first-on-off')
+let firstBlurOn = document.getElementById('first-blur-on')
+let firstBlurOff = document.getElementById('first-blur-off')
 let firstImage = document.getElementById('first-image')
 
-let secondBlurChanger = document.getElementById('second-blur-changer')
-let secondOnOff = document.getElementById('second-on-off')
+let secondBlurOn = document.getElementById('second-blur-on')
+let secondBlurOff = document.getElementById('second-blur-off')
 let secondImage = document.getElementById('second-image')
+
+let thirdBlurOn = document.getElementById('third-blur-on')
+let thirdBlurOff = document.getElementById('third-blur-off')
+let thirdImage = document.getElementById('third-image')
+
+let fourthBlurOn = document.getElementById('fourth-blur-on')
+let fourthBlurOff = document.getElementById('fourth-blur-off')
+let fourthImage = document.getElementById('fourth-image')
+
+let fifthBlurOn = document.getElementById('fifth-blur-on')
+let fifthBlurOff = document.getElementById('fifth-blur-off')
+let fifthImage = document.getElementById('fifth-image')
+
+let blurArray = [
+    [firstBlurOn, firstBlurOff],
+    [secondBlurOn, secondBlurOff],
+    [thirdBlurOn, thirdBlurOff],
+    [fourthBlurOn, fourthBlurOff],
+    [fifthBlurOn, fifthBlurOff]
+]
 
 let blurState = 'blurred'
 
-const blurClick = (onOff, image) => {
+const blurClick = (blurOn, blurOff, image) => {
     if (blurState === 'blurred') {
-        onOff.style.transition = 'transform 200ms ease-in-out'
-        onOff.style.transform = 'rotate(180deg) translatex(-80%)'
+        blurOn.style.transition = 'opacity 200ms ease-in-out'
+        blurOn.style.opacity = '1'
+        blurOff.style.transition = 'opacity 200ms ease-in-out'
+        blurOff.style.opacity = '0'
         image.style.transition = 'filter 200ms ease-in-out'
         image.style.filter = 'blur(0px)'
         blurState = 'unblurred'
     } else if (blurState === 'unblurred') {
-        onOff.style.transition = 'transform 200ms ease-in-out'
-        onOff.style.transform = ''
+        blurOn.style.transition = 'opacity 200ms ease-in-out'
+        blurOn.style.opacity = '0'
+        blurOff.style.transition = 'opacity 200ms eaes-in-out'
+        blurOff.style.opacity = '1'
         image.style.transition = 'filter 200ms ease-in-out'
         image.style.filter = 'blur(20px)'
         blurState = 'blurred'
     }
 }
 
-firstBlurChanger.onclick = () => {
-    blurClick(firstOnOff, firstImage)
-}
+blurArray.forEach((numberArray, index) => {
+    if (index === 0) {
+        numberArray.forEach(item => {
+            item.addEventListener('click', () => {
+                blurClick(firstBlurOn, firstBlurOff, firstImage)
+            })
+        })
+    } else if (index === 1) {
+        numberArray.forEach(item => {
+            item.addEventListener('click', () => {
+                blurClick(secondBlurOn, secondBlurOff, secondImage)
+            })
+        })
+    } else if (index === 2) {
+        numberArray.forEach(item => {
+            item.addEventListener('click', () => {
+                blurClick(thirdBlurOn, thirdBlurOff, thirdImage)
+            })
+        })
+    } else if (index === 3) {
+        numberArray.forEach(item => {
+            item.addEventListener('click', () => {
+                blurClick(fourthBlurOn, fourthBlurOff, fourthImage)
+            })
+        })
+    } else if (index === 4) {
+        numberArray.forEach(item => {
+            item.addEventListener('click', () => {
+                blurClick(fifthBlurOn, fifthBlurOff, fifthImage)
+            })
+        })
+    }
+})
 
-firstOnOff.onclick = () => {
-    blurClick(firstOnOff, firstImage)
-}
+let desc = document.querySelectorAll('.desc')
 
-secondBlurChanger.onclick = () => {
-    blurClick(secondOnOff, secondImage)
-}
-
-secondOnOff.onclick = () => {
-    blurClick(secondOnOff, secondImage)
-}
+desc.forEach(descItem => {
+    if (descItem.textContent === 'N/A') {
+        descItem.style.textAlign = 'center'
+    }
+})
